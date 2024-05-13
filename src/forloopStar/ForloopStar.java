c44a4636333ffccde0892580fafe25ae73d1efe7
Khayelihle Sikhumbuzo Innocent Msimango <97398536+21427971@users.noreply.github.com>
2024-05-14 01:19:45 +0200

    Add files via upload

---
+++ src/forloopStar/ForloopStar.java
@@ -0,0 +1,18 @@
+package forloopStar;
+
+public class ForloopStar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // a java program to display a triangle with stars
+     for(int c=0;c<5;c++)
+     {
+    	 for(int v=0;v<c;v++)
+    	 {
+    		 System.out.print("*");
+    	 }
+    	 System.out.println();
+     }
+	}
+
+}
